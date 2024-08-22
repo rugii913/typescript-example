8b40724e70af911b65584e77c11c0e475dfd76af
rugii913 <rugii913@gmail.com>
2024-08-22 21:52:40 +0900

    feat: 재사용 가능한 검증 기능 생성
    재사용 가능한 검증 기능 생성
    - interface를 활용한 Validatable 타입 정의
      - 해당 타입을 받는 검증용 함수 작성

--- drag-and-drop/src/app.ts
+++ drag-and-drop/src/app.ts
@@ -1,3 +1,55 @@
+// validation
+interface Validatable {
+  // 검증할 수 있는 객체를 정의
+  // 객체의 구조를 정의하므로 interface를 사용했지만, 커스텀 타입인 type 혹은 class로 정의하는 것도 가능함
+  value: string | number;
+  // (cf.) ?를 사용하는 대신 boolean | undefined처럼 undefined를 허용할 수도 있음, ?가 같은 역할을 함
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid =true; // 우선 true로 두고 검증을 통과하지 못하면 false를 반환
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0; // &&의 뒤에 있는 expression의 평가 결과가 false라면 isValid는 false가 됨
+  }
+  if (
+    validatableInput.minLength != null &&
+    // - 위에서 이미 length를 확인했기 때문에 minLenth의 값을 확인하는 것은 군더더기일 수 있으나, 안전성을 위해 검사
+    //   - minLength가 falsy인 0이어도 이 검사를 실행하게될 것(validatableInput.minLength라고만 되어 있다면 minLength 값이 0이면 이 검사를 넘어갈 것임)
+    // - !==를 사용한다면 undefined를 명시해줘야 하지만, != null로 undefined를 포함시킴
+    typeof validatableInput.value === "string" // typeof로 타입 가드
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null && typeof validatableInput.value === "number"
+    // min 값이 0일 경우 문제가 생기므로 validatableInput.min처럼 truthy, falsy로 검사하지 않음
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min && Number.isInteger(validatableInput.value);
+    // - Number.isInteger(validatableInput.value) 부분은 강의와 다르게 별도로 추가함
+    // - HTML에서는 <input>의 type="number"로 방지하고 있었으나, HTML만 변경해도 제출할 수 있었음
+  }
+  if (
+    validatableInput.max != null && typeof validatableInput.value === "number"
+    // min 값이 0일 경우 문제가 생기므로 validatableInput.min처럼 truthy, falsy로 검사하지 않음
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max && Number.isInteger(validatableInput.value);
+  }
+  return isValid;
+}
+
 // autobind decorator
 function autobind(
   _target: any,
@@ -49,10 +101,27 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
+    // 만약 Validatable을 class로 정의했다면 new 키워드로 인스턴스화해야할 것
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    }
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    }
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    }
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
     ) {
       alert("Invalid input, please try again!");
       return;
