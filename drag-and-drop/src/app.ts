05d03c8c2a04bdab9378076f6608f7cb5df0b8b9
rugii913 <rugii913@gmail.com>
2024-09-12 23:23:17 +0900

    feat: 드래그앤드롭 구현을 위한 인터페이스 작성
    드래그앤드롭 구현을 위한 인터페이스 작성
    - Draggale, DragTarget 인터페이스 추가

--- drag-and-drop/src/app.ts
+++ drag-and-drop/src/app.ts
@@ -1,3 +1,18 @@
+// interfaces for drag and drop
+// (cf.) 인터페이스는 커스텀 객체 타입을 정의할 때 뿐만 아니라, 클래스에 대한 계약으로 사용할 수 있음
+// - interface Draggable
+interface Draggable {
+  // (cf.) DragEvent는 TS 내장 타입
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+// - interface DragTarget
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void; // 대상이 유효한 드래그 타겟임을 브라우저와 JS에 알림
+  dropHandler(event: DragEvent): void; // 실제 드롭이 발생했을 때 반응
+  dragLeaveHandler(event: DragEvent): void; // 아무 것도 하지 않고 드래그 타겟을 떠났을 때 사용자에게 시각적인 피드백을 주는 것과 관련
+}
+
 // enum ProjectStatus - 사람이 읽어서 이해해야하는 텍스트를 줄이기 위해 enum 사용
 enum ProjectStatus {
   Active,
