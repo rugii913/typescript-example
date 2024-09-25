689e38ab560f27fb011300c1aeca91d1067bf43d
rugii913 <rugii913@gmail.com>
2024-09-25 21:16:56 +0900

    chore: webpack-dev-server 추가, 개발 시 빌드 설정 등
    - webpack-dev-server 추가
      - package.json의 scripts의 start 수정
      - webpack.config.js의 output 수정
      - webpack.config.js의 devServer 프로퍼티 명시
    - 개발 시 빌드 설정
      - webpack.config.js에 mode 명시

--- drag-and-drop/webpack.config.js
+++ drag-and-drop/webpack.config.js
@@ -2,12 +2,21 @@ const path = require("path");
 
 // Webpack이 사용하는 구성 객체가 됨
 module.exports = {
+  mode: "development",
   // 전체 프로젝트가 시작되는 파일(entry point) 명시
   entry: "./src/app.ts",
   // Webpack 동작 결과물 output point 정보를 담은 객체
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
   output: {
     filename: "bundle.js", // 번들링된 .js 파일
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
   devtool: "inline-source-map",
   module: {
