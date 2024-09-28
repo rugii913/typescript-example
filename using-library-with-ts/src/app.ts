b399d2a4ac717f96ac866479224cb87f9f78a91b
rugii913 <rugii913@gmail.com>
2024-09-28 13:41:41 +0900

    feat: TS에서 순수 JS 코드 사용-최후의 수단 declare
    TS에서 순수 JS 코드 사용-최후의 수단 declare
    - @types가 없는 라이브러리, script에 선언된 JS 전역 변수 등을 사용할 때 declare를 이용하여 TS가 이름을 알 수 있도록 함

--- using-library-with-ts/src/app.ts
+++ using-library-with-ts/src/app.ts
@@ -1,3 +1,14 @@
 import _ from "lodash";
 
-console.log(_.shuffle([1, 2, 3]));
+declare const GLOBAL_VARIABLE: string;
+
+let body = document.querySelector("body")!;
+
+const lodashShuffle = _.shuffle([1, 2, 3]);
+const lodashShuffleSpan = document.createElement("span");
+lodashShuffleSpan.innerText = lodashShuffle.toString();
+body.insertAdjacentElement("beforeend", lodashShuffleSpan);
+
+const globalVariableSpan = document.createElement("span");
+globalVariableSpan.innerText = GLOBAL_VARIABLE;
+body.insertAdjacentElement("beforeend", globalVariableSpan);
