f41c73929e5872d102cafc5cf8b5a349164435de
rugii913 <rugii913@gmail.com>
2024-10-02 21:08:27 +0900

    feat: axios, Geocoding API 이용하여 입력된 주소의 좌표 가져오기
    axios, Geocoding API 이용하여 입력된 주소의 좌표 가져오기

--- searching-places/src/app.ts
+++ searching-places/src/app.ts
@@ -1,14 +1,34 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.querySelector("#address")! as HTMLInputElement;
 
 const apiKey = process.env.GOOGLE_API_KEY;
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number; } } }[];
+  status: "OK" | "ZERO_RESULTS";
+}
+
 const searchAddressHandler = (event: Event) => {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  // 제출받은 주소 정보를 구글 API를 이용해 검색할 부분
-  console.log(enteredAddress);
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${apiKey}`
+    )
+    .then(response => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((err: Error) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
