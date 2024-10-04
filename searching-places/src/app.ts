0fc68fb9b7639e5d6f311b3c4f9c85dfdcda7fef
rugii913 <rugii913@gmail.com>
2024-10-05 00:02:11 +0900

    fix: (별도 진행)@types/google.maps로 지도 시작하기
    @types/google.maps로 지도 시작하기
    - 비동기 방식으로 작성해야 함

--- searching-places/src/app.ts
+++ searching-places/src/app.ts
@@ -51,6 +51,14 @@ const apiKey = process.env.GOOGLE_API_KEY;
   // Add other bootstrap parameters as needed, using camel case.
 });
 
+async function initMap(domElementForMap: HTMLElement, startCoodinates: { lat: number, lng: number }): Promise<google.maps.Map> {
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  return new Map(domElementForMap, {
+    center: startCoodinates,
+    zoom: 16,
+  });
+}
+
 // index.html로 직접 불러온 Google maps를 TS에서 사용하기 위해 declare 사용
 // declare var google: any; // @types/googlemaps 패키지를 가져오며 주석 처리
 
@@ -59,26 +67,23 @@ type GoogleGeocodingResponse = {
   status: "OK" | "ZERO_RESULTS";
 }
 
-const searchAddressHandler = (event: Event) => {
+const searchAddressHandler = async (event: Event) => {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   axios
     .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${apiKey}`
-    )
-    .then(response => {
-      if (response.data.status !== "OK") {
-        throw new Error("Could not fetch location!");
-      }
+    ).then(async response => {
+      if (response.data.status !== "OK") throw new Error("Could not fetch location!");
+      
       const coordinates = response.data.results[0].geometry.location;
-      const map = new google.maps.Map(document.getElementById("map")!, {
-        center: coordinates,
-        zoom: 16,
-      });
-      new google.maps.Marker({ position: coordinates, map: map })
-    })
-    .catch((err: Error) => {
+
+      const domElementForMap = document.getElementById("map")!
+      await initMap(domElementForMap, coordinates);
+
+      // new google.maps.Marker({ position: coordinates, map: map }) // @types/google.maps에서는 사용할 수 없어 주석 처리
+    }).catch((err: Error) => {
       alert(err.message);
       console.log(err);
     });
