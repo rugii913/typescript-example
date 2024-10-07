ae335d023558d92215a186ca244b92d391b3736c
rugii913 <rugii913@gmail.com>
2024-10-07 21:02:53 +0900

    feat: 타입 안정성 있는 props를 사용하는 하위 컴포넌트 추가
    타입 안정성 있는 props를 사용하는 하위 컴포넌트 추가
    - React를 TS로 개발할 때 props의 타입을 명시해야함

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+import TodoList from "./components/TodoList";
+
 const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course" }];
+
   return (
-    <div className="App">Hello world!</div>
+    <div className="App">
+      {/* A component that adds todos */}
+      <TodoList items={todos} />
+    </div>
   );
 }
 
