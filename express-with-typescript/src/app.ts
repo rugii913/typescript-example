323fa9fd54c40f4d34a04e1a29fd510b3bb20c5e
rugii913 <rugii913@gmail.com>
2024-11-10 21:13:50 +0900

    feat: routes, controllers 분리, Todo 클래스 정의, todo 등록 API 작성
    - 비즈니스 로직은 controllers 하위로, 라우터는 routes 하위로 코드 분리
      - 각 라우터가 비즈니스 로직을 다루는 함수로 매핑되도록 함
    - todo.ts에 Todo 클래스 정의
    - todo 등록 API 작성
    - app.ts에서 요청의 body를 json으로 파싱하도록 함

--- express-with-typescript/src/app.ts
+++ express-with-typescript/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
+import { json } from "body-parser";
 
 import todoRoutes from "./routes/todos"
 
 const app = express();
+app.use(json()); // 이 미들웨어 역할을 하는 json() 함수는 서버가 수신한 요청의 body를 파싱, JSON 데이터를 추출 → controllers 하위의 함수가 Request 객체의 body에서 적절히 값을 가져올 수 있도록 하는 것
 app.use("/todos", todoRoutes);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
